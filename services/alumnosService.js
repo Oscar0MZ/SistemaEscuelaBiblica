b5210783b6b86d27058112e8c23d1d144dd0c9cd
sroff82 <mateotorrez76@gmail.com>
2026-02-23 22:00:44 -0600

    Add functions to save and retrieve students

--- services/alumnosService.js
+++ services/alumnosService.js
@@ -1,1 +1,23 @@
+import { db } from "../config/firebase.js";
+import { collection, addDoc, getDocs } from "firebase/firestore";
 
+const alumnosCollection = collection(db, "alumnos");
+
+export const guardarAlumno = async (alumno) => {
+  await addDoc(alumnosCollection, alumno);
+};
+
+export const obtenerAlumnos = async () => {
+  const snapshot = await getDocs(alumnosCollection);
+
+  const alumnos = [];
+
+  snapshot.forEach(doc => {
+    alumnos.push({
+      id: doc.id,
+      ...doc.data()
+    });
+  });
+
+  return alumnos;
+};
