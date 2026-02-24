76d91d2d72cba1b62ff26a184c3afc56b618dd09
sroff82 <mateotorrez76@gmail.com>
2026-02-23 23:21:33 -0600

    Add registrarAsistencia function to handle attendance

--- controllers/asistenciaController.js
+++ controllers/asistenciaController.js
@@ -1,1 +1,18 @@
+import {
+    asistenciaYaExiste,
+    guardarAsistencia
+} from "../services/asistenciaService.js";
 
+export async function registrarAsistencia(datos) {
+
+    const existe = await asistenciaYaExiste(datos.fecha, datos.campo);
+
+    if (existe) {
+
+        throw new Error("La asistencia ya fue registrada");
+
+    }
+
+    await guardarAsistencia(datos);
+
+}
