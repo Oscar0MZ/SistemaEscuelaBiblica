18a9b70a90b06439e8b712a3567303de4fb430c3
sroff82 <mateotorrez76@gmail.com>
2026-02-23 22:01:33 -0600

    Add functionality to create and list students

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
+import { crearAlumno, listarAlumnos } from "./controllers/alumnosController.js";
 
+// Crear alumno
+await crearAlumno("Juan", 10, "Masculino");
+
+// Listar alumnos
+const alumnos = await listarAlumnos();
+
+console.log(alumnos);
