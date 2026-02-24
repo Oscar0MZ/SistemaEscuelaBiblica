a064ccbd24e99de04f1834e300a49c9db86df1c9
sroff82 <mateotorrez76@gmail.com>
2026-02-24 12:18:38 -0600

    Update alumnosService.js
    se agrego a alumnos

--- services/alumnosService.js
+++ services/alumnosService.js
@@ -1,1 +1,18 @@
+// services/alumnosService.js
 
+window.AlumnosService = {
+    // Función única para guardar el niño
+    registrar: async (datosAlumno) => {
+        try {
+            // Guardamos en la colección 'alumnos'
+            await window.db.collection('alumnos').add({
+                ...datosAlumno,
+                createdAt: Date.now() // Guardamos cuándo se creó
+            });
+            return true;
+        } catch (error) {
+            console.error("Error guardando alumno:", error);
+            throw error;
+        }
+    }
+};
