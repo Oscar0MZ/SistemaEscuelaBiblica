e5021fb51d7ede3c20305ae13eb677a79a0cbaf8
sroff82 <mateotorrez76@gmail.com>
2026-02-23 21:19:12 -0600

    Update firebase.js

--- config/firebase.js
+++ config/firebase.js
@@ -1,1 +1,22 @@
+// Import Firebase
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
+// Configuración de Firebase
+const firebaseConfig = {
+  apiKey: "TU_API_KEY",
+  authDomain: "TU_AUTH_DOMAIN",
+  projectId: "TU_PROJECT_ID",
+  storageBucket: "TU_STORAGE_BUCKET",
+  messagingSenderId: "TU_MESSAGING_SENDER_ID",
+  appId: "TU_APP_ID"
+};
+
+// Inicializar Firebase
+const app = initializeApp(firebaseConfig);
+
+// Inicializar base de datos
+const db = getFirestore(app);
+
+// Exportar la base de datos
+export { db };
