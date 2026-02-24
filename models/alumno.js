5c2bbee78a7b23c50b68a80b223a3547362b13a8
sroff82 <mateotorrez76@gmail.com>
2026-02-23 22:00:17 -0600

    Add Alumno class with constructor

--- models/alumno.js
+++ models/alumno.js
@@ -1,1 +1,10 @@
+class Alumno {
+  constructor(id, nombre, edad, genero) {
+    this.id = id;
+    this.nombre = nombre;
+    this.edad = edad;
+    this.genero = genero;
+  }
+}
 
+export default Alumno;
