1a0e0bc8d4a5859d1969be0f5dbe23ec3f944d6a
sroff82 <mateotorrez76@gmail.com>
2026-02-23 23:19:59 -0600

    Add Asistencia class with constructor

--- models/asistencia.js
+++ models/asistencia.js
@@ -1,1 +1,14 @@
+export class Asistencia {
 
+    constructor(id, alumnoId, fecha, estado, campo, creadoPor) {
+
+        this.id = id;
+        this.alumnoId = alumnoId;
+        this.fecha = fecha;
+        this.estado = estado;
+        this.campo = campo;
+        this.creadoPor = creadoPor;
+
+    }
+
+}
