619965426dd3144f5e4e0a825bc3564dd74b7984
sroff82 <mateotorrez76@gmail.com>
2026-02-24 00:19:19 -0600

    Implement loginUsuario function for user authentication

--- services/authService.js
+++ services/authService.js
@@ -1,1 +1,29 @@
+import { db } from "../config/firebase.js";
 
+import {
+collection,
+query,
+where,
+getDocs
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+
+export const loginUsuario = async (rol, password) => {
+
+const q = query(
+collection(db, "usuarios"),
+where("rol", "==", rol),
+where("password", "==", password)
+);
+
+const snapshot = await getDocs(q);
+
+if (!snapshot.empty) {
+
+return snapshot.docs[0].data();
+
+}
+
+return null;
+
+};
