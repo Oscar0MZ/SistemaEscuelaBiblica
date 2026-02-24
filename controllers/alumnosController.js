2f01c678f425649f931e67d4a32562d050322a68
sroff82 <mateotorrez76@gmail.com>
2026-02-23 22:01:11 -0600

    Add functions to create and list students

--- controllers/alumnosController.js
+++ controllers/alumnosController.js
@@ -1,1 +1,15 @@
+import { guardarAlumno, obtenerAlumnos } from "../services/alumnosService.js";
 
+export const crearAlumno = async (nombre, edad, genero) => {
+  const alumno = {
+    nombre,
+    edad,
+    genero
+  };
+
+  await guardarAlumno(alumno);
+};
+
+export const listarAlumnos = async () => {
+  return await obtenerAlumnos();
+};
