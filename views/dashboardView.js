668c61228be7a8cf7b872e79f918f61503632b1a
sroff82 <mateotorrez76@gmail.com>
2026-03-02 12:37:58 -0600

    Update dashboardView.js
    nuevo usuario

--- views/dashboardView.js
+++ views/dashboardView.js
@@ -3,15 +3,20 @@ function DashboardView(props) {
         return <window.AdminDashboard {...props} />;
     }
     
-    // --- REDIRECCIÓN PARA LA SECRETARIA ---
     if (props.usuario === 'SECRETARIA') {
         return <window.SecretariaDashboard {...props} />;
+    }
+
+    // --- AQUÍ ESTÁ LA CORRECCIÓN: REDIRECCIÓN DEL TESORERO ---
+    if (props.usuario === 'TESORERO') {
+        return <window.TesoreroDashboard {...props} />;
     }
 
     if (props.usuario === 'LOGISTICA') {
         return <window.LogisticaDashboard {...props} />;
     }
 
+    // Si no es ninguno de los de arriba, asume que es Maestro/Auxiliar
     return <window.MaestroDashboard {...props} />;
 }
 
